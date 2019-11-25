d47f6d7cd15bb911ecab5da3988ecd1b0700d16c
halam <hamxa.alam29@gmail.com>
2019-11-25 16:58:38 +0500

    First Day Commit

--- app/src/main/java/com/glowingsoft/bushdigital/Fragments/Categories.java
+++ app/src/main/java/com/glowingsoft/bushdigital/Fragments/Categories.java
@@ -8,21 +8,28 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.glowingsoft.bushdigital.CategoriesModel;
+import com.glowingsoft.bushdigital.MyApp;
 import com.glowingsoft.bushdigital.PlansPagerAdapter;
 import com.glowingsoft.bushdigital.R;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+import java.util.Objects;
+
+import io.objectbox.Box;
 
 public class Categories extends Fragment {
     TabLayout tab;
     ViewPager viewPager;
     ArrayList<String> tabTitle = new ArrayList<>();
+    Box<CategoriesModel> siteModelBox;
 
     public Categories() {
         // Required empty public constructor
@@ -42,28 +49,19 @@ public class Categories extends Fragment {
         View view = inflater.inflate(R.layout.fragment_categories, container, false);
         tab = view.findViewById(R.id.tabs);
         viewPager = view.findViewById(R.id.frameLayout);
-
+        siteModelBox = ((MyApp) Objects.requireNonNull(getActivity()).getApplication())
+                .getBoxStore()
+                .boxFor(CategoriesModel.class);
         tabTitle.clear();
-        category.clear();
-        category.add("CAMERAS");
-        category.add("BATTERY FLASH SPEEDLITE");
-        category.add("C-STAND");
-        category.add("DIFFUSER");
-        category.add("GIMBAL STABILIZER");
-        category.add("LENSES");
-        category.add("LIGHTING");
-        category.add("MINI JIBS CRANE ");
-        category.add("PHOTOGRAPHY SOFT CONTINUOUS LIGHTING KIT ");
-        category.add("RECORDER");
-        category.add("SANDBAGS");
-        category.add("SLIDER DOLLY");
-        category.add("TRIPOD");
-        category.add("VIDEOMIC");
-        for (int l = 0; l < category.size(); l++) {
 
-            tab.addTab(tab.newTab().setText("" + category.get(l)));
-            tabTitle.add("" + category.get(l));
+
+//        Log.e("TGED", "SIZE" + siteModelBox.getAll());
+        for (int i = 0; i < siteModelBox.getAll().size(); i++) {
+            tab.addTab(tab.newTab().setText("" + siteModelBox.getAll().get(i).getTitle()));
+            tabTitle.add(siteModelBox.getAll().get(i).getTitle());
+
         }
+
         tab.setTabGravity(TabLayout.GRAVITY_CENTER);
         tab.setTabMode(TabLayout.MODE_SCROLLABLE);
         PlansPagerAdapter adapter = new PlansPagerAdapter
