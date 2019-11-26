6b585c4534da9ecb7ad03967268a24880b33ff16
halam <hamxa.alam29@gmail.com>
2019-11-26 16:36:26 +0500

    Second Day Commit

--- app/src/main/java/com/glowingsoft/bushdigital/Fragments/DynamicFragment.java
+++ app/src/main/java/com/glowingsoft/bushdigital/Fragments/DynamicFragment.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,16 +14,35 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.glowingsoft.bushdigital.CategoriesModel;
+import com.glowingsoft.bushdigital.CategoryModelLocal;
+import com.glowingsoft.bushdigital.CategoryModelMainList;
+import com.glowingsoft.bushdigital.MyApp;
 import com.glowingsoft.bushdigital.R;
+import com.glowingsoft.bushdigital.SplashScreenActivity;
 import com.glowingsoft.bushdigital.VideoAdapter;
+import com.squareup.picasso.Picasso;
+
+import net.idik.lib.slimadapter.SlimAdapter;
+import net.idik.lib.slimadapter.SlimInjector;
+import net.idik.lib.slimadapter.viewinjector.IViewInjector;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import io.objectbox.Box;
+import io.objectbox.query.Query;
 
 public class DynamicFragment extends Fragment {
 
     View view;
+    Box<CategoryModelLocal> categoryModelLocalBox;
+    Box<CategoryModelMainList> categoryModelMainListBox;
+    SlimAdapter slimAdapter;
     RecyclerView list;
-VideoAdapter videoAdapter;
+    VideoAdapter videoAdapter;
+
     public static DynamicFragment newInstance(String val) {
         DynamicFragment fragment = new DynamicFragment();
         Bundle args = new Bundle();
@@ -30,19 +51,66 @@ VideoAdapter videoAdapter;
         return fragment;
     }
 
-    ArrayList<String> videoModelArrayList = new ArrayList<>();
+    List<CategoryModelLocal> categorysublist = new ArrayList<>();
+    List<CategoryModelLocal> categoryModelLocals = new ArrayList<>();
+    List<CategoryModelMainList> categorymainlist = new ArrayList<>();
+    List<CategoriesModel> categoriesModels = new ArrayList<>();
     String node;
+    Box<CategoriesModel> siteModelBox;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dynamic, container, false);
         node = getArguments().getString("someValue");
+        categoryModelLocalBox = ((MyApp) Objects.requireNonNull(getActivity()).getApplication())
+                .getBoxStore()
+                .boxFor(CategoryModelLocal.class);
+        categoryModelMainListBox = ((MyApp) Objects.requireNonNull(getActivity()).getApplication())
+                .getBoxStore()
+                .boxFor(CategoryModelMainList.class);
+        siteModelBox = ((MyApp) Objects.requireNonNull(getActivity()).getApplication())
+                .getBoxStore()
+                .boxFor(CategoriesModel.class);
+        Log.e("TGED", "SIZE" + node);
         list = view.findViewById(R.id.list);
-        videoModelArrayList.clear();
-        videoAdapter = new VideoAdapter(getActivity(), node);
-        list.setLayoutManager(new LinearLayoutManager(getActivity()));
-        list.setAdapter(videoAdapter);
+        categorysublist.clear();
+        categorymainlist.clear();
+        categoriesModels.clear();
+        categoryModelLocals.clear();
+        categorymainlist = categoryModelMainListBox.getAll();
+        categorysublist = categoryModelLocalBox.getAll();
+        categoriesModels = siteModelBox.getAll();
+
+//                for (int k = 0; k < categorysublist.size(); k++) {
+//                    if (node.equalsIgnoreCase(categorysublist.get(k).getCategory())) {
+        categoryModelLocals = categorysublist;
+//                        break;
+//
+//                    }
+//
+//
+//        }
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        list.setLayoutManager(linearLayoutManager);
+        slimAdapter = SlimAdapter.create()
+                .register(R.layout.row_item_videp, new SlimInjector<CategoryModelLocal>() {
+                    @Override
+                    public void onInject(final CategoryModelLocal data, IViewInjector injector) {
+                        if (node.equalsIgnoreCase(data.getCategory())) {
+                            injector.text(R.id.product_des, "" + data.getDescription());
+                            injector.text(R.id.product_name, "" + data.getTitle());
+                            injector.with(R.id.product_image, new IViewInjector.Action() {
+                                @Override
+                                public void action(View view) {
+                                    Picasso.get().load(data.getImage()).into((ImageView) view);
+                                }
+                            });
+                        }
+                    }
+                })
+
+                .attachTo(list).updateData(categoryModelLocals);
 
 
         return view;
